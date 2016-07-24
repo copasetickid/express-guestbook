ee658a658b33fd6dbec44fc37a29cf0201195f45
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-07-24 19:25:10 -0400

    Fix typos and formatting

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-var http = require('http'),
-    path = require('path'),
-    express = require('express'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser');
+var http = require("http");
+var path = require("path");
+var express = require("express");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
 
 var app = express();
 
-app.set('views', path.resolve(__dirname, 'views'));
-app.set('view enginer', 'ejs');
-
-var entries = []; //Global array to store all entries
+var entries = [];
 app.locals.entries = entries;
 
-app.use(logger('dev'));
+app.use(logger("dev"));
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", function(request, response) {
-  response.render('index');
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
-app.get("/new-entry", function(request, response) {
-  response.render('new-entry');
+app.get("/new-entry", function(req, res) {
+  res.render("new-entry");
 });
 
-app.post('new-entry', function(request, response) {
-  if(!request.body.title || !request.body.body) {
-    response.status(400).send('Entries must have a title and a body.');
+app.post("/new-entry", function(req, res) {
+  if (!req.body.title || !req.body.body) {
+    res.status(400).send("Entries must have a title and a body.");
+    return;
   }
-
   entries.push({
-    title: request.body.title,
-    content: request.body.body,
+    title: req.body.title,
+    body: req.body.body,
     published: new Date()
   });
-
-  response.redirect('/');
+  res.redirect("/");
 });
 
-app.use(function(request, response) {
-  response.status(404).render('404');
+app.use(function(req, res) {
+  res.status(404).render("404");
 });
 
 http.createServer(app).listen(3000, function() {
